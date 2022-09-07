68f6ac223602bc8930e5c7b30804f861d6a19af9
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-09-07 22:20:02 +0300

    Блок Features (update) 7.09.2022

--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./button.css";
 
-function Button({ children }) {
+function Button({ children, minWidth }) {
 	return (
-		<button type="button" className="button">
+		<button type="button" style={{ minWidth: minWidth }} className="button">
 			{children}
 		</button>
 	);
