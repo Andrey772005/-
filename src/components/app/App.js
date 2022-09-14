5fd6efe2371d4dd76c7a111c056b0ff60600ec2f
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-09-14 22:43:14 +0300

    Mocks 14.09.2022

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PageWrapper from "../layout/page-wrapper/page-wrapper";
 import "./App.css";
+import cardList from "../mocks/cardList";
 
 export default function App() {
-  return <PageWrapper />;
+  return <PageWrapper card={cardList} />;
 }
