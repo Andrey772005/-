1b2de2d157eefa3414dc7fea94903dc21d59850b
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-09-25 19:29:37 +0300

    Styled Components 25.09.2022 (button update)

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Title, { TitleSize } from "../../ui/title/title";
+import Title, {TitleLevel, TitleSize} from "../../ui/title/title";
 import "./about.css";
 
 function About() {
@@ -7,7 +7,7 @@ function About() {
 		<>
 			<section className="about">
 				<div className="about__wrapper">
-					<Title size={TitleSize.BIG}>Магазин фермерских продуктов c доставкой</Title>
+					<Title size={TitleSize.BIG} level={TitleLevel.H1}>Магазин фермерских продуктов c доставкой</Title>
 					<p className="about__text">Все продукты изготавливаются под заказ. Фермеры начинают
 						готовить продукты за день до отправки заказа клиентам.
 						Именно поэтому мы принимаем заказы заранее и доставляем
